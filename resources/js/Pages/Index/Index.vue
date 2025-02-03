899d91f787718f7e8cee4a5b7a82c82432e10ada
David95406 <83376469+David95406@users.noreply.github.com>
2025-02-03 09:23:25 +0100

    init

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from 'vue'
+
+const counter = ref(0)
+const add = (() => counter.value++)
+</script>
+
+<template>
+  <main>
+    <h1>Hello World!</h1>
+    <Button @click="add">Click me {{ counter }}</Button>
+  </main>
+</template>
+
+<style scoped>
+  
+</style>
