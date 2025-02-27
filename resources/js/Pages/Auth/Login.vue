e74f14ab37200d0b14d8c74101f5e421ec07489b
David95406 <83376469+David95406@users.noreply.github.com>
2025-02-27 11:14:11 +0100

    TCRM-80: controller, routes

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useForm } from '@inertiajs/vue3';
+
+const loginForm = useForm({
+    email: null,
+    password: null,
+    phone_number: null
+})
+const login = (() => {
+    loginForm.post(route('login.store'))
+})
+</script>
+
+<template>
+    <header>
+        <h1>Login page</h1>
+    </header>
+    <main>
+
+        <form @submit.prevent="login"></form>
+
+    </main>
+    <footer></footer>
+</template>
+
+<style scoped></style>
