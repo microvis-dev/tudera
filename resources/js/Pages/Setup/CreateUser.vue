fc385b90d81d6f5f5f266e7a763a6422f6cc4860
gicziAttila <83330738+gicziAttila@users.noreply.github.com>
2025-03-10 13:59:19 +0100

    TCRM-81 CreateUser optimized; CreateWorkspace responsibility fixed, workspaceUrl check

--- resources/js/Pages/Setup/CreateUser.vue
+++ resources/js/Pages/Setup/CreateUser.vue
@@ -34,84 +34,53 @@ const submitForm = () => {
 </script>
 
 <template>
-    <div class="flex flex-col h-full items-center px-4 md:px-10 lg:px-20">
-        <header class="w-full max-w-screen-md pt-10 pb-6 flex flex-col items-start">
-            <p class="text-sm font-medium text-gray-400">1/2</p>
-            <h1 class="text-3xl md:text-4xl font-bold text-white">Let's get to know you</h1>
-
-            <div class="flex flex-col md:flex-row items-center py-5 space-y-4 md:space-y-0 md:space-x-5">
-                <div class="bg-blue-600 w-20 h-20 rounded-full"></div>
-                <div class="flex flex-col">
-                    <h2 class="font-bold text-lg mb-2">Profile picture</h2>
-                    <div class="flex space-x-2">
-                        <button class="border border-gray-600 rounded-md px-4 py-2 flex items-center space-x-2 hover:ring-2 hover:ring-gray-500 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#757575" class="bi bi-upload">
-                                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
-                                <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z" />
-                            </svg>
-                            <span>Upload image</span>
-                        </button>
-                        <button class="border border-gray-600 text-gray-500 rounded-md px-5 py-2" disabled>Remove</button>
-                    </div>
-                    <p class="text-xs font-bold text-gray-400 mt-2">*.png, *.jpg files up to 10MB at least 400px by 400px</p>
+    <div class="flex flex-col h-full w-full items-center p-8 md:py-10 md:px-20">
+        <header class="w-full max-w-screen">
+            <section class="mb-5">
+                <p class="ms-0.5 md:ms-0.7 text-sm roboto-font-regular text-[#B3B3B3]">1/2</p>
+                <h1 class="text-2xl md:text-4xl roboto-font-bold">Let's get to know you</h1>
+            </section>
+            <section class="flex flex-col md:flex-row">
+                <div class="flex flex-col items-center md:mt-5">
+                    <img src="https://placehold.co/600x400/blue/blue" alt="Company Logo" title="Company Logo"
+                        class="w-20 h-20 object-cover rounded-full" />
                 </div>
-            </div>
+                <div class="md:ms-5">
+                    <div class="flex flex-col">
+                        <h2 class="text-lg roboto-font-bold py-2">Profile picture</h2>
+                        <div class="flex flex-row">
+                            <button class="flex flex-row border border-gray-600 roboto-font-medium rounded-md px-4 py-2 items-center hover:ring-2 hover:ring-gray-500 focus:outline-none"><img src="../../../assets/upload.svg" class="mt-1 me-1"><span class="">Upload image</span></button>
+                            <button class="border border-gray-600 rounded-md px-4 py-2 roboto-font-medium items-center ms-5 disabled:text-gray-500" disabled>Remove</button>
+                        </div>
+                    </div>
+                    <p class="roboto-font-bold text-[#B3B3B3] text-xs mt-2 block">*.png, *.jpg files up to 10MB at least 400px by 400px</p>
+                </div>
+            </section>
         </header>
-
-        <main class="w-full max-w-screen-md">
-            <form @submit.prevent="submitForm" class="flex flex-col space-y-5">
-                <div class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-                    <div class="flex flex-col w-full">
-                        <label for="first-name" class="text-gray-400 text-sm font-medium">First name</label>
-                        <input
-                            id="first-name"
-                            v-model="name.first"
-                            type="text"
-                            placeholder="Enter your first name..."
-                            class="w-full px-3 py-2 bg-[#1C1D21] text-white border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500"
-                        />
+        <main class="w-full max-w-screen mt-5">
+            <section>
+                <form>
+                    <div class="flex flex-col mb-5">
+                        <label class="text-[#B3B3B3] roboto-font-regular">First name</label>
+                        <input type="text" class="px-3 py-2 bg-[#1C1D21] border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500 text-sm" placeholder="Enter your first name...">
                     </div>
-                    <div class="flex flex-col w-full">
-                        <label for="last-name" class="text-gray-400 text-sm font-medium">Last name</label>
-                        <input
-                            id="last-name"
-                            type="text"
-                            v-model="name.last"
-                            placeholder="Enter your last name..."
-                            class="w-full px-3 py-2 bg-[#1C1D21] text-white border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500"
-                        />
+                    <div class="flex flex-col mb-5">
+                        <label class="text-[#B3B3B3] roboto-font-regular">Last name</label>
+                        <input type="text" class="px-3 py-2 bg-[#1C1D21] border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500 text-sm" placeholder="Enter your last name...">
                     </div>
-                </div>
-
-                <div class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-                    <div class="flex flex-col w-full">
-                        <label for="tel" class="text-gray-400 text-sm font-medium">Phone</label>
-                        <input
-                            id="tel"
-                            type="tel"
-                            v-model="userForm.phone"
-                            placeholder="Enter your phone..."
-                            class="w-full px-3 py-2 bg-[#1C1D21] text-white border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500"
-                        />
+                    <div class="flex flex-col mb-5">
+                        <label class="text-[#B3B3B3] roboto-font-regular">Phone</label>
+                        <input type="tel" class="px-3 py-2 bg-[#1C1D21] border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500 text-sm resize-none" placeholder="Enter your phone number...">
                     </div>
-                    <div class="flex flex-col w-full">
-                        <label for="password" class="text-gray-400 text-sm font-medium">Password</label>
-                        <input
-                            id="password"
-                            type="password"
-                            v-model="userForm.password"
-                            placeholder="Enter your password"
-                            class="w-full px-3 py-2 bg-[#1C1D21] text-white border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500"
-                        />
+                    <div class="flex flex-col mb-5">
+                        <label class="text-[#B3B3B3] roboto-font-regular">Password</label>
+                        <input type="password" class="px-3 py-2 bg-[#1C1D21] border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500 text-sm resize-none" placeholder="Enter your password...">
                     </div>
-                </div>
-
-                <div class="w-full">
-                    <button type="submit" class="bg-blue-600 text-white font-medium rounded-lg px-8 py-2 w-full mt-4">
-                        Next
-                    </button>
-                </div>
-            </form>
+                    <div>
+                        <button type="submit" class="bg-blue-600 shadow-lg roboto-font-regular rounded-lg py-2 w-full hover:bg-blue-700 focus:outline-none hover:shadow-blue-500/50 hover:ring-4">Next</button>
+                    </div>
+                </form>
+            </section>
         </main>
     </div>
 </template>
--- resources/js/Pages/Setup/CreateWorkspace.vue
+++ resources/js/Pages/Setup/CreateWorkspace.vue
@@ -1,8 +1,21 @@
 <script setup>
+import { computed, ref } from "vue";
 import AuthLayout from "../../Layout/AuthLayout.vue";
 import { useForm } from '@inertiajs/vue3';
 import { route } from 'ziggy-js';
 import CreateWorkspaceForm from '../../Components/CreateWorkspaceForm.vue';
+
+
+let workspaceHandle = ref("");
+
+const getWorkspaceUrl = computed(() => {
+    let str = "tudera.com/" + workspaceHandle.value
+    return str.normalize("NFD")
+             .replace(/[\u0300-\u036f]/g, "")
+             .replace(/\s+/g, '-')
+             .toLowerCase();
+
+})
 
 defineOptions({
     layout: AuthLayout
@@ -21,45 +34,46 @@ const SUBMIT_ROUTE = 'setup.workspace.store'
 </script>
 
 <template>
-    <div class="flex flex-col h-full w-full items-center p-8">
+    <div class="flex flex-col h-full w-full items-center p-8 md:py-10 md:px-20">
         <header class="w-full max-w-screen">
             <section class="mb-5">
-                <p class="ms-0.5 md:ms-0.7 text-sm roboto-font-light text-[#B3B3B3]">2/2</p>
-                <h1 class="text-2xl md:text-3xl roboto-font-bold">Create your workspace</h1>
+                <p class="ms-0.5 md:ms-0.7 text-sm roboto-font-regular text-[#B3B3B3]">2/2</p>
+                <h1 class="text-2xl md:text-4xl roboto-font-bold">Create your workspace</h1>
             </section>
             <section class="flex flex-col md:flex-row">
-                <div class="flex flex-col items-center">
+                <div class="flex flex-col items-center md:mt-5">
                     <img src="https://placehold.co/600x400/blue/blue" alt="Company Logo" title="Company Logo"
                         class="w-20 h-20 object-cover rounded-full" />
                 </div>
-                <div class="flex flex-col">
-                    <h2 class="text-lg roboto-font-bold py-2">Company Logo</h2>
-                    <div class="flex flex-row">
-                        <button class="flex flex-row border border-gray-600 roboto-font-medium rounded-md px-4 py-2 items-center hover:ring-2 hover:ring-gray-500 focus:outline-none"><img
-                                src="../../../assets/upload.svg" class="mt-1 me-1"><span class="">Upload image</span></button>
-                        <button class="border border-gray-600 rounded-md px-4 py-2 roboto-font-medium items-center ms-5 disabled:text-gray-500" disabled>Remove</button>
+                <div class="md:ms-5">
+                    <div class="flex flex-col">
+                        <h2 class="text-lg roboto-font-bold py-2">Workspace Logo</h2>
+                        <div class="flex flex-row">
+                            <button class="flex flex-row border border-gray-600 roboto-font-medium rounded-md px-4 py-2 items-center hover:ring-2 hover:ring-gray-500 focus:outline-none"><img src="../../../assets/upload.svg" class="mt-1 me-1"><span class="">Upload image</span></button>
+                            <button class="border border-gray-600 rounded-md px-4 py-2 roboto-font-medium items-center ms-5 disabled:text-gray-500" disabled>Remove</button>
+                        </div>
                     </div>
+                    <p class="roboto-font-bold text-[#B3B3B3] text-xs mt-2 block">*.png, *.jpg files up to 10MB at least 400px by 400px</p>
                 </div>
-                <p class="roboto-font-bold text-[#B3B3B3] text-xs mt-2">*.png, *.jpg files up to 10MB at least 400px by 400px</p>
             </section>
         </header>
         <main class="w-full max-w-screen mt-5">
             <section>
                 <form>
                     <div class="flex flex-col mb-5">
-                        <label class="text-sm text-[#B3B3B3] roboto-font-regular">Company name</label>
-                        <input type="text" class="px-3 py-2 bg-[#1C1D21] border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500 text-sm" placeholder="Enter your company name...">
+                        <label class="text-[#B3B3B3] roboto-font-regular">Workspace name</label>
+                        <input type="text" v-model="workspaceHandle" class="px-3 py-2 bg-[#1C1D21] border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500 text-sm" placeholder="Enter your workspace name...">
                     </div>
                     <div class="flex flex-col mb-5">
-                        <label class="text-sm text-[#B3B3B3] roboto-font-regular">Workspace handle</label>
-                        <input type="text" class="px-3 py-2 bg-[#1C1D21] border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500 text-sm" placeholder="tudera.com/my-workspace">
+                        <label class="text-[#B3B3B3] roboto-font-regular">Workspace handle</label>
+                        <input type="text" :value="getWorkspaceUrl" disabled class="px-3 py-2 bg-[#1C1D21] border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 text-gray-500 text-sm">
                     </div>
                     <div class="flex flex-col mb-5">
-                        <label class="text-sm text-[#B3B3B3] roboto-font-regular">Tax address</label>
-                        <input type="text" class="px-3 py-2 bg-[#1C1D21] border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500 text-sm" placeholder="Enter your tax address">
+                        <label class="text-[#B3B3B3] roboto-font-regular">Description (optional)</label>
+                        <textarea class="px-3 py-2 bg-[#1C1D21] border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 placeholder-gray-500 text-sm resize-none" rows="3" placeholder="Describe your workspace..."></textarea>
                     </div>
                     <div>
-                        <button type="submit" class="bg-blue-600 roboto-font-regular rounded-lg py-2 w-full">Finish</button>
+                        <button type="submit" class="bg-blue-600 shadow-lg roboto-font-regular rounded-lg py-2 w-full hover:bg-blue-700 focus:outline-none hover:shadow-blue-500/50 hover:ring-4">Finish</button>
                     </div>
                 </form>
             </section>
