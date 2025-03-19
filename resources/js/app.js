5c4eb2973b4d58e7d7e89ae1f361dbbd8b984b50
preilakos <preilakos@gmail.com>
2025-03-20 00:29:58 +0100

    TCRM-84: Vuetify & shadcn/ui kivezetése

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,31 +2,18 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { ZiggyVue } from 'ziggy-js'
 import MainLayout from './Layout/MainLayout.vue'
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
     const page =  pages[`./Pages/${name}.vue`]
 
-    page.default.layout = page.default.layout || MainLayout 
+    page.default.layout = page.default.layout || MainLayout
 
     return page
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
-      .use(vuetify)
       .use(ZiggyVue)
       .mount(el)
   },
