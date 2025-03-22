238bc7c64e7f8280829138ccb9e3ebcf29e8434f
David95406 <83376469+David95406@users.noreply.github.com>
2025-03-22 21:01:54 +0100

    TCRM-109: models, migrations

---
+++ app/Models/Calendar.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Calendar extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'workspace_id',
+        'user_id',
+        'title',
+        'start_date',
+        'end_date',
+    ];
+
+    public function workspace()
+    {
+        return $this->belongsTo(Workspace::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_03_22_195431_create_todo_lists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todo_lists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('is_done')->default(false);
+            $table->dateTime('start_date')->nullable();
+            $table->dateTime('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todo_lists');
+    }
+};
