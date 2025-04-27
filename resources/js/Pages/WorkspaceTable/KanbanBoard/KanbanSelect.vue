b9e5405141510e7acdcf1e4af96965aff9b35a24
gicziAttila <83330738+gicziAttila@users.noreply.github.com>
2025-04-27 15:19:50 +0200

    TCRM-122 kanban select

--- resources/js/Pages/WorkspaceTable/KanbanBoard/KanbanSelect.vue
+++ resources/js/Pages/WorkspaceTable/KanbanBoard/KanbanSelect.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { cloneVNode, computed, ref } from "vue"
-import { useTuderaStore } from "@/resources/js/state/state"
-import Kanban from "./Kanban.vue"
+import { computed, ref } from "vue";
+import { useTuderaStore } from "@/resources/js/state/state";
+import Kanban from "./Kanban.vue";
 
 const props = defineProps({
     status_options: Array,
@@ -11,33 +11,61 @@ const props = defineProps({
     },
     columns: Array,
     values: Array
-})
+});
 
-const emit = defineEmits(['hide-table', 'back'])
+const emit = defineEmits(['hide-table', 'back']);
 
-const tuderaState = useTuderaStore()
+const tuderaState = useTuderaStore();
 
-const selectedKanban = ref(props.status_options[0])
-const selectKanban = ((index) => {
-    selectedKanban.value = props.status_options[index]
-    emit('hide-table')
-})
+const selectedKanban = ref(null);
+
+// Computed: Disable "Switch to Kanban version" only if nothing selected
+const isPlaceholderDisabled = computed(() => selectedKanban.value === null);
+
+const selectKanban = () => {
+  if (selectedKanban.value === null) {
+    emit('back'); // If selecting "Switch to Kanban version" again, go back
+  } else {
+    emit('hide-table'); // If a kanban selected, hide table
+  }
+};
 
 const back = () => {
-    emit('back')
-}
+    selectedKanban.value = null;
+    emit('back');
+};
 
 const handleUpdate = (event) => {
-    
-}
-
+  // You can fill this in later if you want to handle updates from Kanban
+};
 </script>
 
 <template>
-    <div v-for="(kanban, index) in status_options" class="flex flex-col w-full h-full">
-        <button @click="selectKanban(index)" class="bg-blue-600">{{ kanban.name }}</button><br>
-    </div>
-    <Kanban v-if="show" :selectedKanban="selectedKanban" :values="values" :columns="columns" @back="back()" @update="handleUpdate" />
+  <select 
+    v-model="selectedKanban" 
+    @change="selectKanban" 
+    class="bg-[#5D5E5B] peer rounded-md px-3 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-blue-600">
+    
+    <option :value="null" :disabled="isPlaceholderDisabled">
+      {{ isPlaceholderDisabled ? 'Switch to Kanban version' : 'Go back' }}
+    </option>
+
+    <option 
+      v-for="kanban in props.status_options" 
+      :key="kanban.name" 
+      :value="kanban">
+      {{ kanban.name }}
+    </option>
+  </select>
+
+  <Kanban 
+    v-if="show" 
+    :selectedKanban="selectedKanban" 
+    :values="values" 
+    :columns="columns" 
+    @back="back()" 
+    @update="handleUpdate" 
+  />
 </template>
 
 <style scoped></style>
