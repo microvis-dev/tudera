ff2142cb3c0b900d28321dde476a5580c823b4c2
preilakos <preilakos@gmail.com>
2025-05-11 20:05:32 +0200

    TCRM-127: init MessagingService.php

---
+++ app/Services/MessagingService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Models\Workspace;
+use App\Models\WorkspaceTable;
+
+class MessagingService
+{
+    public const TYPE_NOTIFICATION = 'notification';
+    public const TYPE_EMAIL = 'email';
+
+    private static function send()
+    {
+        // body
+    }
+    public static function sendToUser()
+    {
+        // body
+    }
+
+    public static function sendToWorkspace(Workspace $workspace)
+    {
+        // body
+    }
+}
+?>
